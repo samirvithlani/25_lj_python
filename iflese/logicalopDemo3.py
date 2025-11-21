53783cf7bbf66dca373e479906c7beb0c7494204
samirvithlani <samir.vithlani83955@gmail.com>
2025-11-21 17:56:20 +0530

    and or combo

---
+++ iflese/logicalopDemo3.py
@@ -0,0 +1,11 @@
+#and or comb
+
+age = 25
+salary =100000
+crediScore = 600
+
+#(salary>=35000 or age>=30) any of them..
+if crediScore>=700 and (salary>=35000 or age>=30):
+    print("you will get loan")
+else:
+    print("you will not get loan")    
