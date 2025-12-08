b7863da0c140f0ed639aa5b7d2db3374db64a41a
samirvithlani <samir.vithlani83955@gmail.com>
2025-12-08 19:01:24 +0530

    task

---
+++ for/forTask1.py
@@ -0,0 +1,25 @@
+sp = int(input("enter sp :"))
+ep = int(input("enter ep :"))
+sum=0
+for i in range(sp,ep+1):
+    print(i,end=" ")
+    #0 = 0 +1 =1
+    #1 = 1 + 2 = 3
+    #3 = 3+ 3 = 6
+    sum = sum+i
+
+
+print("sum - ",sum)
+
+#get 1 no from user and find facorial 
+#5  = 5 * 4 *3 *2 1 = 120
+
+#5
+# 5 * 1 = 5
+
+#fiboonaci series
+# a = 0
+# b =1
+
+#0 1 1 2 3 5 8 13 21
+#hint swap logic
