30fd5cc1501b9b935b4a47641bdcef49be0f85c9
samirvithlani <samir.vithlani83955@gmail.com>
2025-12-08 18:44:52 +0530

    for

---
+++ for/ForDemo1.py
@@ -0,0 +1,20 @@
+#1 to 9
+for i in range(1,10):
+    print(i,end=" ")
+print()
+#0 -->10
+for i in range(11):
+    print(i,end=" ")    
+
+print()
+#1 incremnt 2 ->3 5,7, 19
+for i in range(1,20,2):
+    print(i,end=" ")    
+
+print()
+
+for i in range(10,-1,-1):
+    print(i,end=" ") 
+
+
+#get sp and ep from user eg 1 20 -- print 1 to 20  --> and do sum       
