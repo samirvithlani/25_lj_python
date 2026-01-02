d7e2fd0fa991ae7deffdb86e05e2edd1cbfc8b01
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-02 18:54:37 +0530

    immutable

---
+++ string/StringDemo2.py
@@ -0,0 +1,5 @@
+data = "python"
+print(data)
+print(data[0])
+data[0] = "P" #TypeError: 'str' object does not support item assignment
+print(data)
---
+++ string/StringDemo3.py
@@ -0,0 +1,30 @@
+#string concat
+
+name1 = "abc"
+name2 = "pqr"
+name3 = ""
+#name3 = name1 + name2
+name3 = name3 +"a"
+print(name3)
+
+#
+# data = "ajay"
+# data[0] = data[0]+"A"
+# print(data)
+
+data = "ajay"
+#print(data)
+#data = "a"+b
+data = data[0] + "b"
+print(data)
+
+
+data1 = "ram"
+data2 = "seeta"
+
+# data2[0] = data1[0]
+# print(data1)
+# print(data2)
+
+data3 = data1[0] + data2[1]
+print(data3)
