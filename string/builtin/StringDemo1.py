3d43304cee24dc4f1e41e992d5748f68c2539ac8
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-09 18:32:00 +0530

    strip

---
+++ string/builtin/StringDemo1.py
@@ -0,0 +1,17 @@
+#transformation..
+data = "java"
+print(data)
+data = data.upper()
+print(data)
+data = data.lower()
+print(data)
+
+
+data = "hi this is india ok"
+print(data)
+#data = data.capitalize()
+data = data.title()
+print(data)
+
+data = data.swapcase()
+print(data)
---
+++ string/builtin/StringDemo2.py
@@ -0,0 +1,20 @@
+email = "  samir@gmail.com  " #15
+print(f"email = {email}")
+print(f"len = {len(email)}")
+
+#remove extra white spaces..
+# email  = email.lstrip()
+# print(f"email = {email}")
+# print(f"len = {len(email)}")
+
+# email  = email.rstrip()
+# print(f"email = {email}")
+# print(f"len = {len(email)}")
+
+
+email  = email.strip()
+print(f"email = {email}")
+print(f"len = {len(email)}")
+
+
+
