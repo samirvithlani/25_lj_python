dea05f7dbaad4d68e2742a270dbf0e52ee532a11
samirvithlani <samir.vithlani83955@gmail.com>
2026-02-27 18:51:16 +0530

    data

--- ds/dict/comprehension/dictComp2.py
+++ ds/dict/comprehension/dictComp2.py
@@ -11,3 +11,18 @@ nameswithpalin = {i:"palindrome" if i == i[::-1] else "not palndrome" for i in n
 print(nameswithpalin)
 
 #numbers = [25....] #even odd
+
+#marks = {"Samir": 85, "Rahul": 40, "Aman": 72}
+#dict
+#result = {"Samir":"pass","Rahul":"Fails"}
+
+
+
+#data = {"a": 1, "b": 2, "c": 3}
+#data ={1:"a",2:"b",3:"c"}
+
+#data = {"name": "samir", "city": "ahmedabad"}
+#{"NAME": "samir", "CITY": "ahmedabad"}
+
+#remove Negative Values
+#data = {"a": 10, "b": -5, "c": 20, "d": -2}
