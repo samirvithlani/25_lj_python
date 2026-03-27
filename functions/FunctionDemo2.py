81dea685cdd4bfd1bafdd37262a423783f64e918
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-27 18:45:23 +0530

    function

---
+++ functions/FunctionDemo2.py
@@ -0,0 +1,13 @@
+def avg(a,b,c):
+    print("average function called...")
+    # ans = (a + b +c )/3
+    # return ans
+    return (a + b +c) /3
+
+x = avg(10,20,30)
+print("average = ",x)
+
+#3 function
+#1) accept 2 string argumenrt and retuen full Name
+ #fname virat lname kohli virat kohli
+#2)accept list as argument and return sum of list  dont use builtin sum function..
