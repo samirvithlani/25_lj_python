01969ad14356ddebe0648d8e20c15b3488e28a15
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-27 18:34:17 +0530

    functions

---
+++ ds/set/setDemo7.py
@@ -0,0 +1,9 @@
+#create school data school has 3 std 10 11 12 every std has 5 students 
+
+#1)find common name student from all std
+#2) give all students name
+#3))give only unique students name
+
+
+#we have 7 days sales data 3 time data morning,ev,noon
+#find which day[monday,tue...] has heighest sales
---
+++ functions/FunctionDemo1.py
@@ -0,0 +1,20 @@
+def demo():
+    print("demo function called...")
+    print("No retruen type no argument")
+
+demo() #calling part    
+
+
+def add(a,b):
+    print("addition function called...")
+    print(a+b)
+
+#add() #error add() missing 2 required positional arguments: 'a' and 'b'
+#add(10,20) 
+#add("abc","pqr")
+#add(100,"abc") #error..
+#add(False,True) #tricky question No error
+#add(True,100)
+    
+
+    
